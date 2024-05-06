7adbcaa66965c0e5379b1040a77d017661631ce4
HerndonE <EHerndon17@gmail.com>
2024-05-05 18:08:50 -0600

    [fix] initial upload

---
+++ my-react-app/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import "./styles.scss";
+import CustomNav from "./components/CustomNav.js";
+
+
+/*
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque in dictum non consectetur a erat nam at. Urna duis convallis convallis tellus id. Mauris ultrices eros in cursus turpis massa tincidunt. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Mus mauris vitae ultricies leo integer malesuada nunc. Mus mauris vitae ultricies leo integer malesuada nunc vel. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Aliquam eleifend mi in nulla posuere sollicitudin. Porta nibh venenatis cras sed felis eget velit. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Tortor aliquam nulla facilisi cras fermentum. Mi ipsum faucibus vitae aliquet nec ullamcorper.
+
+Nisi vitae suscipit tellus mauris a. Cursus in hac habitasse platea dictumst quisque sagittis purus. A condimentum vitae sapien pellentesque. Sit amet consectetur adipiscing elit ut aliquam purus sit. Adipiscing elit duis tristique sollicitudin nibh. Lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Consequat mauris nunc congue nisi vitae suscipit tellus. Volutpat diam ut venenatis tellus in metus. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Lacus luctus accumsan tortor posuere ac ut. Tempor nec feugiat nisl pretium. Nisl tincidunt eget nullam non nisi. Elementum curabitur vitae nunc sed. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Dui sapien eget mi proin sed libero. Donec massa sapien faucibus et molestie ac feugiat. Massa placerat duis ultricies lacus.
+*/
+
+function App() {
+  const handleClick = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  return (
+    <div className="app-container">
+    <CustomNav
+      li={[
+        ["About", "img/dashboard.svg"],
+        ["Experience", "img/restaurant.svg"],
+        ["Projects", "img/manage user.svg"]
+      ]}
+      handleClick={handleClick} // Pass handleClick function to CustomNav
+    />
+    <div className="sections-container">
+      <div id="about" className="section">
+        <h2>About</h2>
+        <p className='custom-paragraph'>A condimentum vitae sapien pellentesque.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque in dictum non consectetur a erat nam at. Urna duis convallis convallis tellus id. Mauris ultrices eros in cursus turpis massa tincidunt. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Mus mauris vitae ultricies leo integer malesuada nunc. Mus mauris vitae ultricies leo integer malesuada nunc vel. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Aliquam eleifend mi in nulla posuere sollicitudin. Porta nibh venenatis cras sed felis eget velit. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Tortor aliquam nulla facilisi cras fermentum. Mi ipsum faucibus vitae aliquet nec ullamcorper.
+
+Nisi vitae suscipit tellus mauris a. Cursus in hac habitasse platea dictumst quisque sagittis purus. A condimentum vitae sapien pellentesque. Sit amet consectetur adipiscing elit ut aliquam purus sit. Adipiscing elit duis tristique sollicitudin nibh. Lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Consequat mauris nunc congue nisi vitae suscipit tellus. Volutpat diam ut venenatis tellus in metus. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Lacus luctus accumsan tortor posuere ac ut. Tempor nec feugiat nisl pretium. Nisl tincidunt eget nullam non nisi. Elementum curabitur vitae nunc sed. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Dui sapien eget mi proin sed libero. Donec massa sapien faucibus et molestie ac feugiat. Massa placerat duis ultricies lacus.
+</p>
+      </div>
+      <div id="experience" className="section">
+        <h2>Experience</h2>
+        <p className='custom-paragraph'>A condimentum vitae sapien pellentesque.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque in dictum non consectetur a erat nam at. Urna duis convallis convallis tellus id. Mauris ultrices eros in cursus turpis massa tincidunt. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Mus mauris vitae ultricies leo integer malesuada nunc. Mus mauris vitae ultricies leo integer malesuada nunc vel. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Aliquam eleifend mi in nulla posuere sollicitudin. Porta nibh venenatis cras sed felis eget velit. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Tortor aliquam nulla facilisi cras fermentum. Mi ipsum faucibus vitae aliquet nec ullamcorper.
+
+Nisi vitae suscipit tellus mauris a. Cursus in hac habitasse platea dictumst quisque sagittis purus. A condimentum vitae sapien pellentesque. Sit amet consectetur adipiscing elit ut aliquam purus sit. Adipiscing elit duis tristique sollicitudin nibh. Lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Consequat mauris nunc congue nisi vitae suscipit tellus. Volutpat diam ut venenatis tellus in metus. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Lacus luctus accumsan tortor posuere ac ut. Tempor nec feugiat nisl pretium. Nisl tincidunt eget nullam non nisi. Elementum curabitur vitae nunc sed. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Dui sapien eget mi proin sed libero. Donec massa sapien faucibus et molestie ac feugiat. Massa placerat duis ultricies lacus.
+</p>
+      </div>
+      <div id="projects" className="section">
+        <h2>Projects</h2>
+        <p className='custom-paragraph'>Lorem ipsum dolor sit amet, vitae suscipit tellus mauris a. Cursus in hac habitasse platea dictumst quisque sagittis purus. A condimentum vitae sapien pellentesque. Sit amet consectetur adipiscing elit ut aliquam purus sit. Adipiscing elit duis tristique sollicitudin nibh. Lorem ipsum dolor sit amet consectetur adipiscing elit duis. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Consequat mauris nunc congue nisi vitae suscipit tellus. Volutpat diam ut venenatis tellus in metus. Morbi tempus iaculis urna id volutpat lacus laoreet. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Lacus luctus accumsan tortor posuere ac ut. Tempor nec feugiat nisl pretium. Nisl tincidunt eget nullam non nisi. Elementum curabitur vitae nunc sed. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Dui sapien eget mi proin sed libero. Donec massa sapien faucibus et molestie ac feugiat. Massa placerat duis ultricies lacus.
+        </p>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/CustomNav.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const CustomNav = ({ li, handleClick  }) => {
+  const [window, setWindow] = useState(false);
+
+  /*
+  let openClose = () => {
+    if (window === false) {
+      setWindow(true);
+    } else {
+      setWindow(false);
+    }
+    */
+
+  return (
+    <nav className="navbar-menu" style={{ width: window === false ? 250 : 60 }}>
+      <h1>Ethan Herndon</h1>
+      <h3>Software Engineer</h3>
+      
+      {/* 
+      <div className="burger" onClick={() => openClose()}>
+        <img src="img/menu.svg" alt="burger" />
+      </div>
+      */}
+      <ul className="navbar__list">
+        {li.map((item, i) => (
+          <div className="navbar__li-box" key={i} onClick={() => handleClick(item[0].toLowerCase())}>
+            <img
+              src={item[1]}
+              alt={item[1]}
+              style={{ paddingLeft: window === false ? 27 : 17 }}
+            />
+            <li
+              className="navbar__li"
+              style={{ display: window === false ? "inline-block" : "none" }}
+            >
+              {item[0]}
+            </li>
+          </div>
+        ))}
+      </ul>
+      
+    </nav>
+  );
+};
+
+export default CustomNav;
